6efd35941159e61856f6109a0bbd90c04963ac47
Daniel <danieltoth0910@gmail.com>
2019-06-17 15:41:35 +0100

    move middlewares to seperate file

---
+++ middleware/index.js
@@ -0,0 +1,30 @@
+var Blog      = require ("../models/blog");
+var Comment   = require("../models/comment");
+var User      = require ("../models/user");
+// ========
+// MIDDLEWARE GOES here
+// =============
+
+
+var middlewareObj = {};
+
+middlewareObj.isLoggedIn = function (req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
+
+middlewareObj.checkAdminAuth = function (req, res, next){
+  if(req.isAuthenticated()){
+    User.findById(req.params.body, function (err, foundUser){
+      if (err){
+        console.log(err);
+      } else {
+        console.log
+      }
+    })
+  }
+}
+
+module.exports = middlewareObj ;
