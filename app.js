828d9e5493c02880d71ab9e925137dc06e1cea7b
Daniel Toth <danieltoth@Daniels-MacBook-Pro.local>
2019-09-14 20:27:41 +0100

    Revert "change mongod passowrd env"
    This reverts commit f4ab66b59d39d7c900cd4b25bfd8b282bb5b9984.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var commentRoutes = require ("./routes/comments"),
 // APP CONFIG
 
 // mongoose.connect("mongodb://localhost:27017/ClarissaBlog", { useNewUrlParser: true });
-mongoose.connect("mongodb+srv://L3n1ad:" + process.env.MONGODB_PASSWORD + "@cluster0-9iei5.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://L3n1ad:" + process.env.MONGODBPASSWORD + "@cluster0-9iei5.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
